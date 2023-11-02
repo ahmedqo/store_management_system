d1b3fb9a2338d599101179d0ab0e9de235cee683
ahmedqo <ahmedqo1995@gmail.com>
2023-11-02 20:37:27 +0100

    hopfully last one

--- resources/views/shared/guest/list.blade.php
+++ resources/views/shared/guest/list.blade.php
@@ -3,13 +3,19 @@
 @endphp
 @if ($items)
     <nav class="w-full overflow-auto lg:overflow-unset -mb-2">
-        <ul class="flex gap-2 text-x-black text-md font-medium w-max">
+        <ul class="flex items-center gap-2 text-md font-medium w-max">
             @foreach ($items as $item)
-                <li>
+                <li class="{{ $loop->index < count($items) - 1 ? 'text-[#1D1D1D80]' : 'text-x-black' }}">
                     <a {{ count($item) > 1 ? 'href=' . $item[1] : '' }}>{{ $item[0] }}</a>
                 </li>
                 @if ($loop->index < count($items) - 1)
-                    <li>-</li>
+                    <li>
+                        <svg class="block w-4 h-4 pointer-events-none rtl:rotate-180 text-[#1D1D1D80]" fill="currentcolor"
+                            viewBox="0 -960 960 960">
+                            <path
+                                d="M727-433H145q-21 0-33.5-13T99-479q0-20 12.5-32.5T145-524h583l-66-67q-15-14-15-33t14-33q12-13 31.5-12t33.5 13l145 145q11 13 11 32t-11 32L725-303q-13 14-32 14t-32-13q-14-12-14-32.5t15-33.5l65-65Z" />
+                        </svg>
+                    </li>
                 @endif
             @endforeach
         </ul>
